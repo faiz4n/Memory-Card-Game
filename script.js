a28e2258241f486174b6bd428eaf352d89c0eb0f
Faizan <149582509+faiz4n@users.noreply.github.com>
2025-04-12 17:20:24 +0530

    Add files via upload
    Redesigned layout and implemented timer & flip counter

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
 const cards = document.querySelectorAll('.card');
-const cardContent = ['🍔', '🍔', '❤️', '❤️', '🍀', '🍀', '🚗', '🚗', '🌈', '🌈', '⭐', '⭐', '🐬', '🐬', '⚽️', '⚽️'];
+const cardContent = ['🍔', '🍔', '❤️', '❤️', '🎁','🎁', '🚗', '🚗', '🌈', '🌈', '⭐', '⭐', '🐬', '🐬', '⚽️', '⚽️'];
 const shuffledContent = cardContent.sort((a, b) => Math.random() - 0.5);
 const resetButton = document.querySelector('.restBtn');
+const flipCount = document.querySelector('.flips');
+const timer = document.querySelector('.timer');
 
 
 let firstCardElement = null;
 let firstCardContent = null;
 let secondCardElement = null;
 let secondCardContent = null;
+let flips = 0;
+let time = 60;
 let lockboard = false;
+let WinStatus = false;
+
+const countdown = setInterval(() => {
+    time -= 1;
+    timer.innerHTML = time;
+
+    if(time <= 0){
+        clearInterval(countdown);
+        setTimeout(() => {
+            alert("Time's Up!");
+        }, 100)
+    }
+}, 1000);
+
+
 
 function resetGame() {
     location.reload();
 }
 
 
+
+function checkWin(){
+    const checkCards = [...cards].every((card) =>
+        card.classList.contains('cardMatched')
+    )
+    if(checkCards){
+        clearInterval(countdown)
+        alert("YOU WIN!");
+    }
+}
+
+
 cards.forEach((card, idx) => {
     card.addEventListener('click', () => {
-        if( lockboard || card.innerHTML != "?"){ return }
+        if( lockboard || card.innerHTML != "?" || time <= 0){ return }
+        flips += 1;
+        flipCount.innerHTML = flips;
         
         card.innerHTML = shuffledContent[idx];
         
@@ -30,7 +63,7 @@ cards.forEach((card, idx) => {
             secondCardElement = card;
         }
         console.log(firstCardContent, secondCardContent);
-
+        
         if (firstCardContent != null && secondCardContent != null) {
             lockboard = true;
             if (firstCardContent != secondCardContent) {
@@ -40,21 +73,23 @@ cards.forEach((card, idx) => {
                     firstCardElement.innerHTML = "?";
                     secondCardElement.innerHTML = "?";
                     lockboard = false;
-                }, 500)
+                }, 350)
                 
             }
             else if(firstCardContent === secondCardContent){
                 firstCardContent = null;
                 secondCardContent = null;
-                setTimeout(() => {
-                    firstCardElement.style.backgroundColor="green"
-                    secondCardElement.style.backgroundColor="green"
+                    firstCardElement.classList.add('cardMatched');
+                    secondCardElement.classList.add('cardMatched');
                     lockboard = false;
-                }, 100);
+                    setTimeout(() => checkWin(), 50)
+
             }
         }
-
+        
+        
     })
 })
+
 
 
